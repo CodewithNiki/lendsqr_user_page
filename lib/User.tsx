9a7571c0be6690680e1e20deb44c7cbb8c6e980e
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2023-05-10 17:22:56 +0100

    update

--- lib/User.tsx
+++ lib/User.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function getUsers() {
+export const getUsers = async() =>{
   // Fetch data from an external API endpoint
   const res = await axios.get('https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users');
   if (!res.data) throw new Error('failed to fetch data');
